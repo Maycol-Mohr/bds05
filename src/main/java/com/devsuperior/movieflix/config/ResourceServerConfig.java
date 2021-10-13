e9255acddf686aad974ed363dd48e84e8f3b3d81
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-13 16:34:29 +0200

    correcoes 3

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -16,43 +15,29 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	private Environment env;
-	
-	@Autowired
-	private JwtTokenStore tokenStore;
-	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-	
-	private static final String[] VISITOR_GET = { "/users/1/**" };
-	
-	private static final String[] MEMBER_GET = { "/users/2/**" };
-	
-	
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.tokenStore(tokenStore);
-	}
+@Autowired
+private Environment env;
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		
-		//H2
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
-		
-		
-		//http.authorizeRequests()
-		//.antMatchers(PUBLIC).permitAll()
-		//.antMatchers(HttpMethod.GET, VISITOR_GET).hasAnyRole("VISITOR", "MEMBER")
-		//.anyRequest().authenticated();
-		//.anyRequest().hasAnyRole("MEMBER");
-		
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, VISITOR_GET).hasAnyRole("VISITOR")
-		.antMatchers(HttpMethod.GET, MEMBER_GET).hasAnyRole("MEMBER")
-		.anyRequest().hasAnyRole("MEMBER");
-	}
+@Autowired
+private JwtTokenStore tokenStore;
+
+private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+@Override
+public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+resources.tokenStore(tokenStore);
 }
+
+@Override
+public void configure(HttpSecurity http) throws Exception {
+
+// H2
+if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+http.headers().frameOptions().disable();
+}
+
+http.authorizeRequests()
+.antMatchers(PUBLIC).permitAll()
+.anyRequest().authenticated();
+}
+} 
