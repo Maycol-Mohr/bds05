ee6325db62b33dd33242cd386c5205731a32c4a9
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-13 10:56:38 +0200

    exceptions, validations, security

--- src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.devsuperior.movieflix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.User;
 
-public interface UserRepository extends JpaRepository<Genre, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
 
 }
