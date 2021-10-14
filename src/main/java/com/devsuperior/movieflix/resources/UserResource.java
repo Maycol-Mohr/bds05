7b7df0207cf31a35a6bbe4b896140b4e825e6daa
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-14 15:22:22 +0200

    correcao final correta

--- src/main/java/com/devsuperior/movieflix/resources/UserResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/UserResource.java
@@ -3,27 +3,24 @@ package com.devsuperior.movieflix.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.UserDTO;
-import com.devsuperior.movieflix.services.AuthService;
+import com.devsuperior.movieflix.services.UserService;
+
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	//@Autowired
-	//private UserService service;
-	
-	@Autowired
-	private AuthService authService;
-	
-	@GetMapping(value = "/{profile}")
-	public ResponseEntity<UserDTO> authenticated(@PathVariable Long id) {
-		dto = authService.authenticated();
-		return ResponseEntity.ok().body(dto);
-	}
 
+@Autowired
+private UserService service;
+
+@GetMapping(value = "/profile")
+public ResponseEntity<UserDTO> getProfile() {
+UserDTO dto = service.getProfile();
+return ResponseEntity.ok(dto);
+}
 }
